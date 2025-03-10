be3f8f3f3cb422206a83b1de301a4c10b33cb051
susveronka <sustkova_veronika@oauh.cz>
2025-03-10 07:18:45 +0100

    .

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+       MenuFrame frame = new MenuFrame();
+       frame.setVisible(true);
+    }
+}
---
+++ src/MenuFrame.java
@@ -0,0 +1,5 @@
+import javax.swing.*;
+
+public class MenuFrame extends JFrame{
+    private JPanel panel;
+}
