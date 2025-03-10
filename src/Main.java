e6715bad0ae182bfdabf1bb41f0c1cd883e403a9
susveronka <sustkova_veronika@oauh.cz>
2025-03-10 08:15:06 +0100

    .

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-       MenuFrame frame = new MenuFrame();
-       frame.setVisible(true);
+
     }
 }
--- src/MenuFrame.java
+++ src/MenuFrame.java
@@ -1,5 +1,57 @@
 import javax.swing.*;
+import java.util.Random;
 
-public class MenuFrame extends JFrame{
+public class MenuFrame extends JFrame{ //obsahuje JFrame
     private JPanel panel;
+    private JCheckBox cbZatrzeno;
+    private JTextField textField1;
+
+    Random generator = new Random();
+
+    public MenuFrame(){ // konstrukstor, kde voláme metody
+        initComponents();
+
+
+    }
+
+    public static void main(String[] args) {
+        MenuFrame frame = new MenuFrame(); // vytvoří oobjekt frame
+        frame.setVisible(true); // nastaví abychom viděli okna ve framě
+    }
+
+    private void initComponents(){
+        setContentPane(panel);
+        setTitle("Ukázka menu");
+        setSize(400,200);
+        initMenu();
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+    private void initMenu() {
+        JMenuBar menuBar = new JMenuBar(); //vytvoříme panel pro menu
+        setJMenuBar(menuBar);    //nastavíme panel
+
+        JMenu menuAkce = new JMenu("akce"); // vytvoříme políčko menu
+        menuBar.add(menuAkce); // přidíme políčko do panelu
+
+        JMenuItem miPozdrav = new JMenuItem("pozdrav"); //vytvoří položku
+        menuAkce.add(miPozdrav); //menu přidej položku
+        menuAkce.addSeparator(); // menu přidej oddělovač
+        miPozdrav.addActionListener(e -> pozdrav()); //přidá úkol co provede
+
+
+        JMenuItem miZmeny = new JMenuItem("změna");
+        menuAkce.add(miZmeny);
+        miZmeny.addActionListener(e -> provedZmeny()); //přidá úkol co provede
+    }
+
+    private void pozdrav(){ // metoda pro vyskakovací okno pro položku POZDRAV
+        JOptionPane.showMessageDialog(this, "Ahoj z menu"); //vyskakovací okno Ahoj
+    }
+
+    private void provedZmeny(){ // vyvolá akci pro položku ZMĚNA
+        cbZatrzeno.setSelected(! cbZatrzeno.isSelected()); // zaškrtne opačnou vlastnost
+        textField1.setText(""+(generator.nextInt(100))); // vygeneruje náhodné číslo a dá do textového pole
+
+    }
 }
